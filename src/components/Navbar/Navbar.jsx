5d113221a303b395f634a9172c5e93411127223c
Joshhortt <joshhortt@yahoo.com>
2022-03-07 14:57:57 +0000

    add smallscreen navbar - hamburger with styles.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,6 +23,24 @@ const Navbar = () => (
         <div />
         <a href='/' className='p__opensans'>Book Table</a>
         </div>
+
+        <div className='app__navbar-smallscreen'>
+          <GiHamburgerMenu color='#fff' font-size={27} onClick={() => {}} />
+
+            <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+              <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => {}} />
+
+            </div>
+
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans'><a href='#home'>Home</a></li>
+              <li className='p__opensans'><a href='#about'>About</a></li>
+              <li className='p__opensans'><a href='#menu'>Menu</a></li>
+              <li className='p__opensans'><a href='#awards'>Award</a></li>
+              <li className='p__opensans'><a href='#contact'>Contact</a></li>
+          </ul>
+        </div>
+
   </nav>
 );
 
