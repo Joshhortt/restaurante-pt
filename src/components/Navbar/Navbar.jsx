c6d94c507f59b8eae599e976d7140bfe814241c2
Joshhortt <joshhortt@yahoo.com>
2022-03-07 13:35:18 +0000

    Add react imports, navbar & navbar links; add styles to navbar.css' and logo.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import images from '../../constants/images';
 
 import './Navbar.css';
-
+{/*note that app__navbar has 2 underscores. BEM - Block Element Modifier*/} 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    <div className="app__navbar">
+      <img src={images.gericht} alt="app logo" />
+      </div>
+        <ul className='app__navbar-links'>
+          <li className='p__opensans'><a href='#home'>Home</a></li>
+          <li className='p__opensans'><a href='#about'>About</a></li>
+          <li className='p__opensans'><a href='#menu'>Menu</a></li>
+          <li className='p__opensans'><a href='#awards'>Award</a></li>
+          <li className='p__opensans'><a href='#contact'>Contact</a></li>
+        </ul>
+  </nav>
 );
 
 export default Navbar;
