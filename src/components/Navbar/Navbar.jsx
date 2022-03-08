ddd6f39be412aaa0df5bea1dbb06f2f51c316a81
Joshhortt <joshhortt@yahoo.com>
2022-03-08 13:08:27 +0000

    add styles to navbar smallscreen links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import images from '../../constants/images';
 import './Navbar.css';
 {/*note that app__navbar has 2 underscores. BEM - Block Element Modifier*/} 
 const Navbar = () => {
-  {/*Add react state*/} 
+ 
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
@@ -32,9 +32,9 @@ const Navbar = () => {
               
               {toggleMenu && (
               <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-                <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
+                <MdOutlineRestaurantMenu font-Size={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
               
-              <ul className='app__navbar-smallscreen-links'>
+              <ul className='app__navbar-smallscreen_links'>
                 <li className='p__opensans'><a href='#home'>Home</a></li>
                 <li className='p__opensans'><a href='#about'>About</a></li>
                 <li className='p__opensans'><a href='#menu'>Menu</a></li>
@@ -43,7 +43,7 @@ const Navbar = () => {
             </ul>
           </div>
           )}
-          
+
         </div>
     </nav>
   )
