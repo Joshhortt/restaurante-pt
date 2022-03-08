1e35c178b02535c467d71b30ff7a922bb358b9f4
Joshhortt <joshhortt@yahoo.com>
2022-03-08 14:09:08 +0000

    add subHeading' and image.

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+
+import { images } from '../../constants';
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chase the new flavour'/>
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+    </div>
+
+    <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
