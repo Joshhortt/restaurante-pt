5320f74649135301868648eafb7eb190914c173c
Joshhortt <joshhortt@yahoo.com>
2022-03-09 13:51:41 +0000

    add about us' logic and styles.

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 
-import { images} from '../../constants';
+import { images } from '../../constants';
 
 import './AboutUs.css';
 
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about' >
-    <div classname='app__aboutus-overlay flex__center'>
+    <div className='app__aboutus-overlay flex__center'>
       <img src={images.PB} alt='pb letters' />
     </div>
 
-    <div classname='app__aboutus-content flex__center'>
-      <div classname='app__aboutus-about_aboutus'>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
         <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type='button' className='custom__button'>Know More</button>
       </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt='about_knife' />
+      </div>
+      
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
     </div>
   </div>
 );
