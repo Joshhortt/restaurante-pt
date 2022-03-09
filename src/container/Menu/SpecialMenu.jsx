d8abebe4ec18e530d286f43f1c75031f265df94c
Joshhortt <joshhortt@yahoo.com>
2022-03-09 14:28:06 +0000

    add h1' className to Special Menu section.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -4,8 +4,11 @@ import { images, data } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte" />
+      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
+    </div>
   </div>
 );
 
