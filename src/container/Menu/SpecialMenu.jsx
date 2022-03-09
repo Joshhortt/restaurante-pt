a29dfd19eb486f811e298f6c3984a79e603cb101
Joshhortt <joshhortt@yahoo.com>
2022-03-09 14:44:33 +0000

    add View More' button.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -31,7 +31,9 @@ const SpecialMenu = () => (
           ))}
         </div>
       </div>
-
+    </div>
+    <div style={{ marginTop: 15 }}>
+      <button type="button" className="custom__button">View More</button>
     </div>
   </div>
 );
