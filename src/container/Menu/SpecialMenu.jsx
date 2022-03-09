973204e19ae3000916bbdb0b6267bb557f65ae9b
Joshhortt <joshhortt@yahoo.com>
2022-03-09 14:17:34 +0000

    initialize SpecialMenu section by adding imports.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
