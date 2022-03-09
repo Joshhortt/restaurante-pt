1bd4be30b4ff361ef20e8dfbd03b6e7e47e91f62
Joshhortt <joshhortt@yahoo.com>
2022-03-09 14:36:21 +0000

    add menu items and callback function map'.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -9,6 +9,17 @@ const SpecialMenu = () => (
       <SubHeading title="Menu that fits your palatte" />
       <h1 className="headtext__cormorant">Today&apos;s Special</h1>
     </div>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine  flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine, index) => (
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
 
