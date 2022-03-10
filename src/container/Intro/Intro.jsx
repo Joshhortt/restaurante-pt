0adb2f067da83a7ffa9f42d93b045537f60faeaf
Joshhortt <joshhortt@yahoo.com>
2022-03-10 14:37:51 +0000

    add onClick' property, conditions play/pause icons

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -18,14 +18,26 @@ const Intro = () => {
         controls={false}
         muted
       />
-
         <div className="app__video-overlay flex__center">
-          <div className="app__video-overlay_circle flex__center">
+          <div className="app__video-overlay_circle flex__center"
+           onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
+          >
+            {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
           </div>
          </div>
-  
   </div>
-)
-  }
+);
+  };
 
 export default Intro;
