acbf950d777f70414aa8c1fe76dfc81658cd537c
Joshhortt <joshhortt@yahoo.com>
2022-03-10 13:29:30 +0000

    add image to section Chef'.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -5,7 +5,9 @@ import './Chef.css';
 
 const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
-    Chef
+     <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
   </div>
 );
 
