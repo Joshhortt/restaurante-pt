1c2d92a78eb85b9490578fcdb0805070e3a461a5
Joshhortt <joshhortt@yahoo.com>
2022-03-10 14:53:57 +0000

    Initialize Laurels' section, add imports & wrapper

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+  <div className="app__wrapper_info">
+    
+  </div>
   </div>
 );
 
