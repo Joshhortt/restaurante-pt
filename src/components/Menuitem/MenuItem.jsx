116919ced065565b1191bdbe05dc79efc2c77211
Joshhortt <joshhortt@yahoo.com>
2022-03-10 12:25:14 +0000

    add to Menu Item, parameter 'price'.

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = ({ title }) => (
+const MenuItem = ({ title, price }) => (
   <div className="app__menuitem">
     <div className="app__menuitem-head">
       <div className="app__menuitem-name">
         <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
       </div>
+
+      <div className="app__menuitem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+
     </div>
   </div>
 );
