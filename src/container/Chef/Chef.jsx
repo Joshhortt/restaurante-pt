0b02a0e509d659704ace2a58e32611f021927e3e
Joshhortt <joshhortt@yahoo.com>
2022-03-10 13:32:14 +0000

    add subheading and h1' to section.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -8,6 +8,12 @@ const Chef = () => (
      <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={images.chef} alt="chef_image" />
     </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+    </div>
+
   </div>
 );
 
