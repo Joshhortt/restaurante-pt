13cb52ca6095b463cdb5b31d81fc8f236665f8cc
Joshhortt <joshhortt@yahoo.com>
2022-03-11 12:48:23 +0000

    add scrollRef from useRef React component.

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -5,6 +5,9 @@ import { images } from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => {
+
+  const scrollRef = React.useRef(null);
+
   return (
   <div className="app__gallery flex__center">
     <div className="app__gallery-content">
@@ -12,6 +15,11 @@ const Gallery = () => {
       <h1 className="headtext__cormorant">Photo Gallery</h1>
       <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
       <button type="button" className="custom__button">View More</button>
+    </div>
+
+    <div className="app__gallery-images">
+      <div className="app__gallery-images_container" ref={scrollRef}>
+      </div>
     </div>
   </div>
 );
