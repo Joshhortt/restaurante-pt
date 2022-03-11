d7692db701c053e05938037d00f2e7ce879644af
Joshhortt <joshhortt@yahoo.com>
2022-03-11 13:45:23 +0000

    correct typos in Gallery section files.

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => {
-
   const scrollRef = React.useRef(null);
 
   const scroll = (direction) => {
@@ -26,7 +26,6 @@ const Gallery = () => {
         <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
         <button type="button" className="custom__button">View More</button>
       </div>
-
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
           {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
@@ -41,10 +40,8 @@ const Gallery = () => {
           <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
-      
     </div>
   );
 };
-
 
 export default Gallery;
