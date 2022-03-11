9b2dfae870cbca8d45468e7ff2dac12a91ec95c2
Joshhortt <joshhortt@yahoo.com>
2022-03-11 15:00:26 +0000

    add footer links contact details to Footer.jsx'.

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,6 +8,14 @@ const Footer = () => (
   <div className="app__footer section__padding" id="login">
     <FooterOverlay />
     <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">Tv. da Audiência n 1, 7800-393 Beja, Portugal - EU</p>
+        <p className="p__opensans">+351 964 438 030</p>
+      </div>
+  </div>
   </div>
 );
 
