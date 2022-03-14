4eaf5fb511f90f7f2052ee85ead1a607122ac0a8
Joshhortt <joshhortt@yahoo.com>
2022-03-14 14:46:55 +0000

    Add more contact details to Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,5 +1,8 @@
+
+
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
 import { FooterOverlay, Newsletter } from '../../components';
 import { images } from '../../constants';
 import './Footer.css';
@@ -12,12 +15,16 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">Tv. da Audiência n 1, 7800-393 Beja, Portugal</p>
+        <p className="p__opensans">Travessa da Audiência nº 1</p>
+        <p className="p__opensans">7800-393 Beja</p>
+        <p className="p__opensans">Centro Histórico</p>
+        <p className="p__opensans">Baixo Alentejo - Portugal</p>
         <p className="p__opensans">+351 964 438 030</p>
+        <p className="p__opensans">+351 284 000 000</p>
       </div>
 
       <div className="app__footer-links_logo">
-      <img src={images.peterbrissos} alt="footer_logo" />
+        <img src={images.peterbrissos} alt="footer_logo" />
         <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
         <img src={images.spoon} alt="footer_spoon" className="spoon__img" style={{ marginTop: 15 }} />
         <div className="app__footer-links_icons">
@@ -29,17 +36,21 @@ const Footer = () => (
 
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Sunday-Thursday:</p>
+        <p className="p__opensans">Monday-Thursday:</p>
         <p className="p__opensans">11:00 am - 11:00 pm</p>
         <p className="p__opensans">Friday-Saturday:</p>
         <p className="p__opensans">11:00 am - 12:00 am</p>
+        <p className="p__opensans">Sunday:</p>
+        <p className="p__opensans">11:00 am - 11:00 pm</p>
+        <p className="p__opensans">National Holidays:</p>
+        <p className="p__opensans">Closed</p>
       </div>
-      
-
-      <div className="footer__copyright">
-      <p className="p__opensans">2022 Peter Brissos. All Rights reserved.</p>
-     </div>
     </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2021 Peter Brissos. All Rights reserved.</p>
+    </div>
+
   </div>
 );
 
