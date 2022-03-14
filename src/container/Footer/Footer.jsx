be1ff5a6af89d484f4200fa27cc32af7efe6ef00
Joshhortt <joshhortt@yahoo.com>
2022-03-14 12:35:48 +0000

    add footer links logo' & social icons' to Footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -17,7 +17,14 @@ const Footer = () => (
       </div>
 
       <div className="app__footer-links_logo">
-
+      <img src={images.peterbrissos} alt="footer_logo" />
+        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
       </div>
 
       <div className="app__footer-links_work">
